e9800284e44d9c57ca1669bb75bc86e1971f9791
learn <learn@LEARN-19.local>
2020-02-14 13:32:38 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -28,6 +28,37 @@ export default class App extends Component{
   }
 
   // Add a method here that returns information to your user by adding a key:value pair to the state object. This method should utilize the user inputs established in state.
+checkMatch = () => {
+    let { userName, loveName} = this.state
+    //turn userName into an array
+    let userArr = userName.split("")
+    let loveArr = loveName.split("")
+    let numbers = [1,2,3,4,5,6,7,8,9,0]
+    // select a random index of the array
+    let randomIndex = Math.floor(Math.random()*userArr.length)
+    let userChar = userArr[randomIndex]
+    let loveComp = 0
+    if(/^[a-zA-Z]*$/.test(userName) === false) {
+        this.setState({tempKey: "Numbers not allowed"})
+    } else if(/^[a-zA-Z]*$/.test(loveName) === false){
+        this.setState({tempKey:"Numbers not allowed"})
+    } else {
+    if(loveArr.includes(userChar)){
+        loveComp += 20
+    }
+    if(loveArr.length === userArr.length){
+        loveComp+=20
+    }
+    if(userArr.includes('a') && loveArr.includes('a')){
+        loveComp+=20
+    }
+    if(userArr.includes('y') || loveArr.includes('y')){
+        loveComp-=20
+    }
+    this.setState({tempKey:loveComp})
+    }
+}
+
 
   render(){
 
@@ -44,52 +75,53 @@ export default class App extends Component{
           <h1 id = "title"> Love-O-Meter </h1>
 
           {/* User input field - every DOM event that happens in the input will call the handleChange methods and update state */}
-          <input
-            id = "userName"
-            onChange = { this.handleChangeUser }
-            value = { userName }
-            placeholder = "Your name"
-          />
-          <span id = "plus"> + </span>
-          <input
-            id = "loveName"
-            onChange = { this.handleChangeLove }
-            value = { loveName }
-            placeholder = "Your love's name"
-          />
-          <br/>
-
-          <button
-            id = "submitButton"
-            type = "submit"
-          >
-          Submit!
-          </button>
-
-          <p> Your compatibility percentage! </p>
-
-          <textarea
-            id = "compatibility"
-            placeholder = "???"
-            value = { userName }
-          />
-
+          <div className = "input-field">
+              <input
+                id = "userName"
+                onChange = { this.handleChangeUser }
+                value = { userName }
+                placeholder = "Your name"
+              />
+              <span id = "plus"> + </span>
+              <input
+                id = "loveName"
+                onChange = { this.handleChangeLove }
+                value = { loveName }
+                placeholder = "Your love's name"
+              />
+              <br/>
+              <button
+                id = "submitButton"
+                type = "submit"
+                onClick={this.checkMatch}
+              >
+              Submit!
+              </button>
+              </div>
+              <p> Your compatibility percentage! </p>
+              <div className = "field">
+              <textarea
+                id = "compatibility"
+                placeholder = "???"
+                value = { this.state.tempKey}
+              />
+              </div>
           {/* Conditional rendering, based on the return value of calculatePercentage */}
           {/* Feel free to go in and change the values here to fall in line with your desired matching criteria */}
-          { tempKey <= 10 && tempKey > 0 &&
+          { tempKey <= 20 &&
             <div>
               <BadMatch />
             </div>
           }
 
-          { tempKey > 10 &&
+          { tempKey > 20 &&
             <div>
               <GoodMatch />
             </div>
           }
 
           {/* Go ahead and customize this info! */}
-          <footer> [Dev name here] | LEARN Academy Alpha 2020 </footer>
+          <footer> Rudy & Art | LEARN Academy Alpha 2020 </footer>
         </div>
 
       </>
